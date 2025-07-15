a82a8b9cc4584fb17ffbe88a1101a744154ec836
조혜린 <hyer1205@gmail.com>
2025-07-15 15:57:58 +0900

    ✨ Feat: [LTS] 102. Binary Tree Level Order Traversal 문제 풀이

---
+++ Lucy/src/2025-7-W02/102_Binary_Tree_Level_Order_Traversal.ts
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+// 2ms
+function levelOrder(root: TreeNode | null): number[][] {
+  if (!root) return [];
+
+  const result: number[][] = [];
+  const queue: TreeNode[] = [root];
+
+  while (queue.length > 0) {
+    const currentLevel = queue.length;
+    const currentNodes: number[] = [];
+
+    for (let i = 0; i < currentLevel; i++) {
+      const node = queue.shift();
+
+      if (!node) continue;
+
+      currentNodes.push(node.val);
+
+      if (node.left) {
+        queue.push(node.left);
+      }
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+
+    result.push(currentNodes);
+  }
+
+  return result;
+}
