06202b6bf451ef711d9ecfdc5cdade1f013d79f6
Seori15 <wlstn6278@gmail.com>
2025-08-15 13:48:25 +0900

    ✨ Feat: [LTS] 909. Snakes and Ladders 문제 재풀이

--- Seori/909. Snakes and Ladders.py
+++ Seori/909. Snakes and Ladders.py
@@ -1,32 +1,26 @@
-from collections import deque
-from typing import List
-
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
+        # [1] 보드판 설정
         n = len(board)
-        def get_pos(s):
-            quot, rem = divmod(s, n)
-            row = n - 1 - quot
-            col = rem if quot % 2 == 0 else n - 1 - rem
-            return row, col
-
-        visited = [False] * (n * n + 1)
-        queue = deque()
-        queue.append((1, 0))  # (칸 번호, 이동 횟수)
-        visited[1] = True
-
+        board.reverse()
+        for i in range(n):
+            if i % 2:
+                board[i].reverse()
+                
+        goal = n ** 2
+        visited = [0] * goal
+        queue = [0]
         while queue:
-            s, moves = queue.popleft()
-            if s == n * n:
-                return moves
-            for i in range(1, 7):
-                next_s = s + i
-                if next_s > n * n:
-                    continue
-                r, c = get_pos(next_s - 1)
-                if board[r][c] != -1:
-                    next_s = board[r][c]
-                if not visited[next_s]:
-                    visited[next_s] = True
-                    queue.append((next_s, moves + 1))
-        return -1
+            now = queue.pop(0)
+            now_count = visited[now]
+            for move in range(1, 7):
+                next = now + move
+                if next >= goal: continue
+                next_i, next_j = next // n, next % n
+                if board[next_i][next_j] != -1:
+                    next = board[next_i][next_j] - 1
+                if visited[next]: continue
+                visited[next] = now_count + 1
+                queue.append(next)
+
+        return visited[-1] if visited[-1] else -1
