79c18e1ad6b626587057b18fd1b7552c15d7cecd
조혜린 <hyer1205@gmail.com>
2025-09-04 12:50:00 +0900

    ✨ Feat: [LTS] 36. valid sudoku 문제 풀이
    - using Array<Set<string>>

--- Lucy/src/2025-9-W02/36_valid_sudoku.ts
+++ Lucy/src/2025-9-W02/36_valid_sudoku.ts
@@ -1,3 +1,33 @@
+// using Array<Set<string>>
+function isValidSudoku(board: string[][]): boolean {
+  const n = board.length;
+
+  const rows: Set<string>[] = Array.from({ length: n }, () => new Set<string>());
+  const cols: Set<string>[] = Array.from({ length: n }, () => new Set<string>());
+  const boxes: Set<string>[] = Array.from({ length: n }, () => new Set<string>());
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      const currentCh = board[i][j];
+
+      if (currentCh === ".") continue;
+
+      const boxIndex = Math.floor(i / 3) * 3 + Math.floor(j / 3);
+
+      if (rows[i].has(currentCh) || cols[j].has(currentCh) || boxes[boxIndex].has(currentCh)) {
+        return false;
+      }
+
+      rows[i].add(currentCh);
+      cols[j].add(currentCh);
+      boxes[boxIndex].add(currentCh);
+    }
+  }
+
+  return true;
+}
+
+// using only for-loop
 function isValidSudoku(board: string[][]): boolean {
   // check sub boxes
   const START_COORDINATES = [
